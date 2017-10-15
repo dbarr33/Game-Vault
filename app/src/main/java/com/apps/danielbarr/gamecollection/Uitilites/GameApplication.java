d1a02879d794651f4423e3a7a35a4e9bb62d3c47
Daniel <dbarr@fuzzpro.com>
2017-10-15 18:27:47 -0400

    add crash reporting.

--- app/src/main/java/com/apps/danielbarr/gamecollection/Uitilites/GameApplication.java
+++ app/src/main/java/com/apps/danielbarr/gamecollection/Uitilites/GameApplication.java
@@ -2,50 +2,51 @@ package com.apps.danielbarr.gamecollection.Uitilites;
 
 import android.app.Activity;
 import android.app.Application;
-
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by danielbarr on 7/6/15.
  */
 public class GameApplication extends Application {
 
-    private static String GOOGLE_ANALYTICS_ID = "UA-76408911-1";
-    public static boolean TURN_ON_ANALYTICS  = false;
+  private static String GOOGLE_ANALYTICS_ID = "UA-76408911-1";
+  public static boolean TURN_ON_ANALYTICS = false;
 
-    public Tracker mTracker;
+  public Tracker mTracker;
 
-    private Tracker setUpGoogleAnalyticsTracking() {
-        GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-        String trackingId;
-            analytics.setLocalDispatchPeriod(30);
-            trackingId = GOOGLE_ANALYTICS_ID;
+  private Tracker setUpGoogleAnalyticsTracking() {
+    GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+    String trackingId;
+    analytics.setLocalDispatchPeriod(30);
+    trackingId = GOOGLE_ANALYTICS_ID;
 
-        Tracker tracker = analytics.newTracker(trackingId);
-        tracker.enableExceptionReporting(true);
-        tracker.enableAutoActivityTracking(false);
-        return tracker;
-    }
+    Tracker tracker = analytics.newTracker(trackingId);
+    tracker.enableExceptionReporting(true);
+    tracker.enableAutoActivityTracking(false);
+    return tracker;
+  }
 
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    mTracker = setUpGoogleAnalyticsTracking();
+    Fabric.with(this, new Crashlytics());
+  }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mTracker = setUpGoogleAnalyticsTracking();
-    }
+  private static Activity activity;
 
-    private static Activity activity;
+  public static Activity getActivity() {
+    return activity;
+  }
 
-    public static Activity getActivity() {
-        return activity;
-    }
+  public static void setActivity(Activity activity) {
+    GameApplication.activity = activity;
+  }
 
-    public static void setActivity(Activity activity) {
-        GameApplication.activity = activity;
-    }
-
-    public static String getResourceString(int id){
-        return activity.getString(id);
-    }
+  public static String getResourceString(int id) {
+    return activity.getString(id);
+  }
 }
